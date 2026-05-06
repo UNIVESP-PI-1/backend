f1c69a06f13608dd10dead8da7efb405c8f47563
Ulisses <ualbardeiro@gmail.com>
2026-05-06 12:07:03 -0300

    Atualizar productSchema com novos campos

--- schemas/product_schema.py
+++ schemas/product_schema.py
@@ -9,6 +9,9 @@ class ProductCreateSchema(BaseModel):
     barcode: Optional[str] = None
     cost_price: int
     sale_price: int
+    stock_quantity = int
+    min_stock = int
+    status = bool
 
 class ProductUpdateSchema(BaseModel):
     name: Optional[str] = None
@@ -18,6 +21,9 @@ class ProductUpdateSchema(BaseModel):
     barcode: Optional[str] = None
     cost_price: Optional[int] = None
     sale_price: Optional[int] = None
+    stock_quantity = Optional[int] = None
+    min_stock = Optional[int] = None
+    status = Optional[bool] = None
 
 class ProductResponseSchema(BaseModel):
     id: int
@@ -28,6 +34,9 @@ class ProductResponseSchema(BaseModel):
     barcode: Optional[str]
     cost_price: int
     sale_price: int
+    stock_quantity = int
+    min_stock = int
+    status = bool
 
     class Config:
         from_attributes = True
